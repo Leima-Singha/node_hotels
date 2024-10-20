82f7dbb429fefd1274ab7261e1ce876ca2e819e0
Leima Singha <leimasingha@Leimas-MacBook-Air.local>
2024-10-20 11:35:08 +0530

    shift mongodb server to online server

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Define MongoDB connection url
-const mongoURL = 'mongodb://localhost:27017/hotels'
+// const mongoURL = 'mongodb://localhost:27017/hotels'
+// const mongoURL = process.env.MONGODB_URL_LOCAL;
+const mongoURL = process.env.MONGODB_URL;
 
 // set up MongoDB connection
 mongoose.connect(mongoURL)
